df2b0e7c53beecb295f45a7dc9f67c5a3b1576b4
HiKoLaaa <hikolaaa2017@yandex.ru>
2021-09-15 15:33:18 +0300

    Add: office tests.

--- OfficeRent.Tests/Database/TestDbContext.cs
+++ OfficeRent.Tests/Database/TestDbContext.cs
@@ -1,3 +1,4 @@
+using System.Threading;
 using Microsoft.EntityFrameworkCore;
 using OfficeRent.Api.Database;
 
@@ -5,13 +6,17 @@ namespace OfficeRent.Tests.Database
 {
 	internal sealed class TestDbContext : DbContext
 	{
+		private static int _databaseNumber = 1;
+
 		public OfficeDbContext DbContext { get; }
 
 		public TestDbContext()
 		{
 			var options = new DbContextOptionsBuilder<OfficeDbContext>()
-				.UseInMemoryDatabase(databaseName: "InMemory")
+				.UseInMemoryDatabase(databaseName: $"InMemory{_databaseNumber}")
 				.Options;
+
+			Interlocked.Increment(ref _databaseNumber);
 
 			DbContext = new OfficeDbContext(options);
 		}
