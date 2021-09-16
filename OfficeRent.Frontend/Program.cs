ee260bb6b54f13301e2443e0d1e275087ad70f85
HiKoLaaa <hikolaaa2017@yandex.ru>
2021-09-16 18:05:59 +0300

    Add: radzen.

--- OfficeRent.Frontend/Program.cs
+++ OfficeRent.Frontend/Program.cs
@@ -1,29 +1,25 @@
 using System;
 using System.Net.Http;
-using System.Collections.Generic;
 using System.Threading.Tasks;
-using System.Text;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 
 namespace OfficeRent.Frontend
 {
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("#app");
+	public class Program
+	{
+		public static async Task Main(string[] args)
+		{
+			var builder = WebAssemblyHostBuilder.CreateDefault(args);
+			builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-            builder
-                .Services
-                .AddOfficeRentClient()
-                .ConfigureHttpClient(client => client.BaseAddress = new Uri(builder.Configuration["GraphQLEndpoint"]));
+			builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+			builder
+				.Services
+				.AddOfficeRentClient()
+				.ConfigureHttpClient(client => client.BaseAddress = new Uri(builder.Configuration["GraphQLEndpoint"]));
 
-            await builder.Build().RunAsync();
-        }
-    }
+			await builder.Build().RunAsync();
+		}
+	}
 }
